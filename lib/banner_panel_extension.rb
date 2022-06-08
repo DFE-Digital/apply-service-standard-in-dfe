6f5d33660998ae5de8ee6137d582f369e6f0d683
LYONS <Kerry.LYONS@education.gov.uk>
2022-06-08 09:08:59 +0100

    change to file at end of day

--- lib/banner_panel_extension.rb
+++ lib/banner_panel_extension.rb
@@ -5,11 +5,13 @@ class BannerPanel < Middleman::Extension
   
     helpers do
       def banner_panel(title, content)
-
-        "<div class='dfeuk-warning-callout'>
+        <<~EOS
+        <div class='dfeuk-warning-callout'>
         <h2 class='dfeuk-warning-callout__label'>#{title}</h2>
         <p>#{content}</p>
-        </div>\n"
+        </div>
+
+        EOS
       
       end
     end

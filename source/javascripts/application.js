291c8be5c62a71b686685bfa6c2a2a93183e1d49
Andy Jones <andy.jones@digital.education.gov.uk>
2022-06-22 08:43:11 +0100

    Homepage changes

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,1 +1,16 @@
 //= require govuk_tech_docs
+
+
+const cards = document.querySelectorAll('.card');
+Array.prototype.forEach.call(cards, card => {
+    let down, up, link = card.querySelector('h2 a');
+    card.style.cursor = 'pointer';
+    card.onmousedown = () => down = +new Date();
+    card.onmouseup = () => {
+        up = +new Date();
+        if ((up - down) < 200) {
+            link.click();
+        }
+    }
+});
+

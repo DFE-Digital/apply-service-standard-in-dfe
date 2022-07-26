567f5a86e9791966e1e23cff793429ac24c0bc37
Andy Jones <andy.jones@digital.education.gov.uk>
2022-07-26 13:20:13 +0100

    styling changes for manual

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -19,3 +19,24 @@ Array.prototype.forEach.call(cards, function (card) {
         }
     };
 });
+
+(function ($, Modules) {
+    'use strict'
+    Modules.AnchoredHeadings = function () {
+      this.start = function ($element) {
+        var headings = $element.find('h1, h2, h3, h4, h5, h6')
+        headings.each(injectAnchor)
+      }
+  
+      function injectAnchor () {
+        var $this = $(this)
+    
+        if ($this.attr('id')) {        
+          $this.addClass('anchored-heading')
+          $this.prepend(
+            '<a href="#' + $this.attr('id') + '" class="anchored-heading__icon" aria-hidden="true" tabindex="-1"></a>'
+          )
+        }
+      }
+    }
+  })(jQuery, window.GOVUK.Modules)

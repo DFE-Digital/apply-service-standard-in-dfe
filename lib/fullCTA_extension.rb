d6a70e5b381a0eed5695f286c4736fc91e0905d3
Andy Jones <andy.jones@digital.education.gov.uk>
2022-08-31 09:21:27 +0100

    styles and tags for standards pages

--- lib/fullCTA_extension.rb
+++ lib/fullCTA_extension.rb
@@ -7,7 +7,9 @@ class CTA < Middleman::Extension
       def fullCta(html)
         <<~EOS
 <div class="fullCTA"> 
-<svg xmlns="http://www.w3.org/2000/svg" height="48" width="48" fill="#ffffff"><path d="m12.1 38 10.5-14-10.5-14h3.7l10.5 14-10.5 14Zm12.6 0 10.5-14-10.5-14h3.7l10.5 14-10.5 14Z"/></svg>#{html}</div>
+<svg xmlns="http://www.w3.org/2000/svg" height="48" width="48" fill="#ffffff"><path d="m12.1 38 10.5-14-10.5-14h3.7l10.5 14-10.5 14Zm12.6 0 10.5-14-10.5-14h3.7l10.5 14-10.5 14Z"/></svg>
+#{html}
+</div>
 EOS
       end
     end

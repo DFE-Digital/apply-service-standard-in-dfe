540ad25b87651d089835a0670f89e9cc8f945a0c
Andy Jones <andy.jones@digital.education.gov.uk>
2022-09-22 11:27:10 +0100

    added clarity

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -39,5 +39,10 @@ Array.prototype.forEach.call(cards, function (card) {
         }
       }
     }
-  })(jQuery, window.GOVUK.Modules)
+  })(jQuery, window.GOVUK.Modules);
 
+(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src='https://www.clarity.ms/tag/'+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, 'clarity', 'script', 'driuzqz382');
